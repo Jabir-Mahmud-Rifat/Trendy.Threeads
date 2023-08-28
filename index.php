9a0d58cb05c03c748527fa957aa6ca6238aa75c7
samiul05 <ssamiul00@gmail.com>
2023-08-28 21:03:54 +0600

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Retail Club - Home</title>
+    <title>Trendy Threads - Home</title>
 
     <script src="https://kit.fontawesome.com/98e008cd61.js" crossorigin="anonymous"></script>
 
@@ -22,7 +22,7 @@
         <header class="header" id="header-index">
             <nav class="nav container">
                 <a href="index.php" class="nav-logo">
-                    <i class="fa-solid fa-vest-patches"></i> Retail Club
+                    <i class="fa-solid fa-vest-patches"></i> Trendy Threads
                 </a>
 
                 <div class="nav-menu" id="nav-menu">
@@ -96,3 +96,231 @@
                 </div>
                 <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
             </div>
+            <div class="product">
+                <img src="https://i.ibb.co/GMCtkxx/foodie-tshirt-image-mydesignation-eat-at-trivandrum-C.jpg" alt="">
+                <div class="description">
+                    <span>Unisex T-shirts</span>
+                    <h5>Foodie T-shirt</h5>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>BDT. 900</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
+            </div>
+            <div class="product">
+                <img src="https://i.ibb.co/FVDnPz5/harry-potter-tshirt-glow-in-the-dark-mydesignation-mockup-glow.gif" alt="">
+                <div class="description">
+                    <span>Glow in the Dark</span>
+                    <h5>Harry Potter T-shirt</h5>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>BDT. 850</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
+            </div>
+            <div class="product">
+                <img src="https://i.ibb.co/vjHxhMq/kgf-tshirt-mockup.jpg" alt="">
+                <div class="description">
+                    <span>Unisex T-shirts</span>
+                    <h5>KGF Rules T-shirt</h5>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>BDT. 820</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
+            </div>
+            <div class="product">
+                <img src="https://i.ibb.co/HpGPRTk/manushyan-tshirt-mydesignation-mockup-latest-image.jpg" alt="">
+                <div class="description">
+                    <span>Unisex T-shirts</span>
+                    <h5>Manushyan T-shirt</h5>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>BDT. 800</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
+            </div>
+            <div class="product">
+                <img src="https://i.ibb.co/9pf6KvS/malayali-tshirt-mydesignation-mockup-image-latest-golden-1.jpg" alt="">
+                <div class="description">
+                    <span>Unisex T-shirts</span>
+                    <h5>Malayali T-shirt</h5>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>BDT. 870</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
+            </div>
+            <div class="product">
+                <img src="https://i.ibb.co/VHj9KXb/theyyam-tshirt-mydesignation-image.jpg" alt="">
+                <div class="description">
+                    <span>Unisex T-shirts</span>
+                    <h5>Theyyam T-shirt</h5>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>BDT. 950</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
+            </div>
+            <div class="product">
+                <img src="https://i.ibb.co/Y2qWWZW/pwoli-tshirt-mydesignation-image-latest.jpg" alt="">
+                <div class="description">
+                    <span>Unisex T-shirts</span>
+                    <h5>Pwoli T-shirt</h5>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>BDT. 850</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="banner">
+        <h4>We Create Quality</h4>
+        <h2>Up to <span>50% off</span> - on All T-shirts and Accessories</h2>
+        <button onclick="window.location='shop.php'">Explore More</button>
+    </section>
+
+    <section class="feature-product">
+        <h2>New Arrivals</h2>
+        <p>We Proudly Present Our New Stock</p>
+        <div class="product-container">
+            
+            <div class="product">
+                <img src="https://i.ibb.co/HpGPRTk/manushyan-tshirt-mydesignation-mockup-latest-image.jpg" alt="">
+                <div class="description">
+                    <span>Unisex T-shirts</span>
+                    <h5>Manushyan T-shirt</h5>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>BDT. 800</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
+            </div>
+            <div class="product">
+                <img src="https://i.ibb.co/9pf6KvS/malayali-tshirt-mydesignation-mockup-image-latest-golden-1.jpg" alt="">
+                <div class="description">
+                    <span>Unisex T-shirts</span>
+                    <h5>Malayali T-shirt</h5>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>BDT. 870</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
+            </div>
+            <div class="product">
+                <img src="https://i.ibb.co/VHj9KXb/theyyam-tshirt-mydesignation-image.jpg" alt="">
+                <div class="description">
+                    <span>Unisex T-shirts</span>
+                    <h5>Theyyam T-shirt</h5>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>BDT. 950</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
+            </div>
+            <div class="product">
+                <img src="https://i.ibb.co/Y2qWWZW/pwoli-tshirt-mydesignation-image-latest.jpg" alt="">
+                <div class="description">
+                    <span>Unisex T-shirts</span>
+                    <h5>Pwoli T-shirt</h5>
+                    <div class="rating">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <h4>BDT. 850</h4>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="sm-banner">
+        <div class="banner-box">
+            <h4>Crazy Deals</h4>
+            <h2>Buy 1 get 1 Free</h2>
+            <span>The Best T-shirts You Can Find</span>
+            <button>Learn More</button>
+        </div>
+
+        <div class="banner-box banner-box2">
+            <h4>Our Collection</h4>
+            <h2>Check it out!</h2>
+            <span>The Best T-shirts You Can Find</span>
+            <button>Learn More</button>
+        </div>
+    </section>
+
+    <!-- footer section -->
+    <footer>
+        <h3>Trendy Threads</h3>
+        <p>For more information on us,<br> please follow the links below</p>
+
+        <div class="socials">
+            <a href="#" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="#" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="fa-brands fa-behance" target="_blank"></i></a>
+            <a href="#"><i class="fa-brands fa-artstation" target="_blank"></i></a>
+        </div>
+
+        <h6>© 2022 Trendy Threads - All Rights Reserved</h6>
+    </footer>
+    
+</body>
+</html>
