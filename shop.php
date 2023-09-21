8b19a426dba1ede992588227b0e2325e1894173c
samiul05 <ssamiul00@gmail.com>
2023-09-21 20:59:20 +0600

    Create shop.php
    Adding new page

---
+++ shop.php
@@ -0,0 +1,81 @@
+<?php
+    include("connection.php");
+     session_start ();  
+    // echo "Welcome  ". $_SESSION['username'] ;  
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product</title>
+
+    <script src="https://kit.fontawesome.com/98e008cd61.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css\style.css">
+</head>
+<body>
+    <header class="header" id="header">
+        <nav class="nav container">
+            <a href="index.php" class="nav-logo">
+                <i class="fa-solid fa-vest-patches"></i> Trendy Threads
+            </a>
+
+            <div class="nav-menu" id="nav-menu">
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="shop.php" class="nav-link active">Shop</a>
+                    </li>
+
+                    <li class="nav-item">
+                            <a href="about.php" class="nav-link">About</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="contact.php" class="nav-link">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="nav-btn">
+
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="login and signup.php" class="login-btn" id="login-btn"><i class="fa-solid fa-user"></i></a>
+                    </li>
+
+                     <li class="nav-item">
+                            <a href="cart.php" class="nav-cart" id="cart-shop""><i class="fa-solid fa-cart-shopping"></i></a>
+                     </li>
+                </ul>
+
+            </div>
+        </nav>
+    </header>
+
+ 
+
+    <!-- footer section -->
+    <footer>
+        <h3>Trendy Threads</h3>
+        <p>For more information on us,<br> please follow the links below</p>
+
+        <div class="socials">
+            <a href="#" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="#" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="fa-brands fa-behance" target="_blank"></i></a>
+            <a href="#"><i class="fa-brands fa-artstation" target="_blank"></i></a>
+        </div>
+
+        <h6>© 2023 Trendy Threads - All Rights Reserved</h6>
+    </footer>
+    
+</body>
+</html>
